511be5eae077bdc0a611367cfc94f083be4443e6
Deepak Patidar <deepak@newput.com>
2018-04-17 17:14:21 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+
+
+import { AppComponent } from './app.component';
+
+import { LoginComponent } from './shared-components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
