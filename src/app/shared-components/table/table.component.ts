79e576de515205dfaafcc15b2f9d356674272f07
Deepak Patidar <deepak@newput.com>
2018-04-24 15:12:05 +0530

    Added css for table

--- src/app/shared-components/table/table.component.ts
+++ src/app/shared-components/table/table.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TableComponent implements OnInit {
 
   optionsSelect: Array<any>;
+  tableData: Array<any>
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +17,12 @@ export class TableComponent implements OnInit {
             { value: '2', label: 'Option 2' },
             { value: '3', label: 'Option 3' },
         ];
+    this.tableData = [
+      { tripId: '101', date: '05/21/2011', type: 'Accepted', name: 'Truner Flight', status: 'Completed', amount: '$5000 USD', view: 'detail'},
+      { tripId: '102', date: '05/23/2017', type: 'Completed', name: 'Truner Flight', status: 'Completed', amount: '$5000 USD', view: 'detail'},
+      { tripId: '103', date: '10/25/2016', type: 'Refunded', name: 'Acme Aviation', status: 'Pending', amount: '$1000 USD', view: 'detail'},
+      { tripId: '104', date: '10/28/2016', type: 'Pending', name: 'Acme Aviation', status: 'Pending', amount: '$1000 USD', view: 'detail'}
+    ];
   }
 
 }
