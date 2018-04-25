46c8e0f94da9d104bd33295a6c9a5014be46ebe9
Deepak Patidar <deepak@newput.com>
2018-04-25 11:04:51 +0530

    Another way to sorting table icon

---
+++ src/app/shared-components/table/sortabletable.directive.ts
@@ -0,0 +1,2 @@
+import { Directive, OnInit, EventEmitter, Output, OnDestroy, Input } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
--- src/app/shared-components/table/table.component.ts
+++ src/app/shared-components/table/table.component.ts
@@ -9,9 +9,11 @@ export class TableComponent implements OnInit {
 
   optionsSelect: Array<any>;
   tableData: Array<any>
+  sortDirection: String;
   constructor() { }
 
   ngOnInit() {
+    this.sortDirection = "asc";
     this.optionsSelect = [
             { value: '1', label: 'Option 1' },
             { value: '2', label: 'Option 2' },
@@ -23,6 +25,29 @@ export class TableComponent implements OnInit {
       { tripId: '103', date: '10/25/2016', type: 'Refunded', name: 'Acme Aviation', status: 'Pending', amount: '$1000 USD', view: 'detail'},
       { tripId: '104', date: '10/28/2016', type: 'Pending', name: 'Acme Aviation', status: 'Pending', amount: '$1000 USD', view: 'detail'}
     ];
-  }
+
+  };
+
+  searchFilter(startDate, endDate, showOnly, searchKeyword) {
+    if((startDate && endDate) || showOnly || searchKeyword) {
+        console.log(startDate);
+        console.log(endDate);
+        console.log(showOnly);
+        console.log(searchKeyword);
+    }
+
+  };
+
+  sort(column, direction) {
+    if(direction === 'asc') {
+      this.sortDirection = 'desc';
+    } else {
+      this.sortDirection = 'asc';
+    }
+    this.tableData.sort((a: any, b: any): any => {
+       return (this.sortDirection == 'asc' ? (a[column] > b[column]) : (a[column] < b[column]));
+     });
+
+  };
 
 }
