eb9ee165d1d443723e166e887e388a2724017d36
Deepak Patidar <deepak@newput.com>
2018-04-27 15:36:40 +0530

    Added operator

--- src/app/shared-components/transaction-table/transaction-table.component.ts
+++ src/app/shared-components/transaction-table/transaction-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../services/http/app.service';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
 
 @Component({
   selector: 'app-transaction-table',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionTableComponent implements OnInit {
 
-  constructor() { }
+    optionsSelect: Array<any>;
+    tableData: Array<any>;
+    sortDirection = "asc";
+    startDate: any;
+    endDate : any;
+    search: string;
+    model: string;
+    model1: any;
 
-  ngOnInit() {
-  }
+    constructor(private appService : AppService) {
+
+    };
+
+
+    ngOnInit() {
+      this.optionsSelect = [
+              { value: '1', label: 'Option 1' },
+              { value: '2', label: 'Option 2' },
+              { value: '3', label: 'Option 3' },
+          ];
+
+      this.appService.getTableData().subscribe(
+        data => {
+          this.tableData = data;
+        },
+        (err: HttpErrorResponse) => {
+          console.log (err);
+        }
+      );
+    }
+
+    searchData(startDate: string, endDate: string, showOnly: string, searchKeyword: any) {
+
+    };
+
+    sortData(key) {
+      this.tableData.sort((a: any, b: any): any => {
+          return (this.sortDirection == 'asc' ? (a[key] > b[key]) : (a[key] < b[key]));
+      });
+      if(this.sortDirection == "asc") {
+        this.sortDirection = "desc";
+      }
+      else {
+        this.sortDirection = "asc";
+      }
+    }
 
 }
